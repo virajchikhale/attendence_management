2e3ddd3a8d346e8e35816b0e44b45761d8559597
Viraj Chikhale <127619412+virajchikhale@users.noreply.github.com>
2024-02-29 08:24:40 +0530

    Update index.php

--- principal/index.php
+++ principal/index.php
@@ -197,7 +197,7 @@ if($_SESSION["user"]==""){
                     <div class="row">
                         <div class="col-md-12">
                             <div class="copyright">
-                                <p>Copyright © 2024 Code Likho. All rights reserved. Template by <a href="http://codelikho.in">CodeLikho</a>.</p>
+                                <p>Copyright © 2024 GPA. All rights reserved. App by <a href="http://codelikho.in">GPA</a>.</p>
                             </div>
                         </div>
                     </div>
