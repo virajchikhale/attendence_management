39101bb7c845ced4adbbbd764540f21726f9fcda
Viraj Chikhale <127619412+virajchikhale@users.noreply.github.com>
2024-02-29 08:22:15 +0530

    Update footer.php

--- teacher/admin_includes/footer.php
+++ teacher/admin_includes/footer.php
@@ -4,7 +4,7 @@
                     <div class="row">
                         <div class="col-md-12">
                             <div class="copyright">
-                                <p>Copyright © 2024 Colorlib. All rights reserved. Template by <a href="http://codelikho.in">CodeLikho</a>.</p>
+                                <p>Copyright © 2024 GPA. All rights reserved. App by <a href="http://codelikho.in">GPA</a>.</p>
                             </div>
                         </div>
                     </div>
